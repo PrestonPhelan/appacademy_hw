0150f1560a7f297361d77d4c2b9df3bef6c26d20
Preston Phelan <preston.phelan@gmail.com>
2016-12-07 09:54:32 -0800

    Added helper methods for authentication into User model

--- w4d3/AppName/app/models/user.rb
+++ w4d3/AppName/app/models/user.rb
@@ -2,7 +2,32 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
   validates :session_token, presence: true
   validates :password_digest, presence: true
-  validates :password, length: {minimum: 6, allow_nil: true}
+  validates :password, length: { minimum: 6, allow_nil: true }
 
   before_validation :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    User
+      .where(username: username)
+      .where(password: password).first
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  attr_reader :password
+
+  def reset_session_token!
+    @session_token = User.generate_session_token
+    self.save
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def password=(pw)
+    self.password_digest = BCrypt::Password.create(pw)
+  end
 end
