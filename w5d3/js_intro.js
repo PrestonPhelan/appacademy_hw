cc09007456a2ffb478a1524100feb76ecbd29eb4
Preston Phelan <preston.phelan@gmail.com>
2016-12-14 08:53:37 -0800

    Elephant and it's tricks

--- w5d3/js_intro.js
+++ w5d3/js_intro.js
@@ -16,13 +16,13 @@ function divByThreeOrFive(n) {
   return ((n % 5 && !(n % 3)) || (n % 3 && !(n % 5)) );
 }
 
-console.log(divByThreeOrFive(6));
+//console.log(divByThreeOrFive(6));
 
 function fizzBuzz(array) {
   array.filter(divByThreeOrFive);
 }
 
-console.log(fizzBuzz([3, 5, 7, 9, 15]));
+//console.log(fizzBuzz([3, 5, 7, 9, 15]));
 
 
 function isPrime(number) {
@@ -60,3 +60,45 @@ function allOrNothing(){
   }
   return true;
 }
+
+
+function printCallback(array) {
+  array.forEach(function(x) {
+    console.log(x);
+  });
+}
+
+function titleize(array, callback) {
+    let titles = array.map(function(name) {
+      return `Mx. ${name} Jingleheimer Schmidt`;
+    });
+
+    callback(titles);
+}
+
+titleize(["Mary", "Brian", "Leo"], printCallback);
+
+
+function Elephant(name, height, tricks) {
+  this.name = name;
+  this.height = height;
+  this.tricks = tricks;
+}
+
+
+Elephant.prototype.trumpet = function() {
+  console.log(`#{this.name} the elephant goes 'phrRRRRRRRRR!!!!!!'`);
+};
+
+Elephant.prototype.grow = function() {
+  this.height += 12;
+};
+
+Elephant.prototype.addTrick(trick) = function() {
+  this.tricks.push(trick);
+};
+
+Elephant.prototype.play = function() {
+  let index =  Math.floor(Math.random() * (this.tricks.length - 1));
+  console.log(`#{this.name} is #{this.tricks[index]}!`)
+};
