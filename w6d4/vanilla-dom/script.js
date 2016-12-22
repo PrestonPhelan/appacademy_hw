11e045ac5f5d69ff94504fc8cc218e74035b1f05
Preston Phelan <preston.phelan@gmail.com>
2016-12-21 17:46:48 -0800

    Interactive add place

--- w6d4/vanilla-dom/script.js
+++ w6d4/vanilla-dom/script.js
@@ -10,6 +10,29 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
+  const placeFormEl = document.getElementById('place-form');
+  placeFormEl.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const placeInputEl = document.getElementById('favorite-input');
+    console.log(placeInputEl);
+    const placeName = placeInputEl.value;
+    console.log(placeName);
+    placeInputEl.value = "";
+
+    // Get the ul of cats.
+    const ul = document.getElementById("sf-places");
+    // create an li element
+    const li = document.createElement("li");
+    // set the text of the li to be the value of the input field
+    li.textContent = placeName;
+
+    ul.appendChild(li);
+  });
+
+
+  // insert it as the last item in the ul.
+
   document.querySelectorAll("#restaurants li").forEach((li) => {
     li.addEventListener("click", toggleLi);
   });
