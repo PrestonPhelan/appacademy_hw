ba5231af3821ed40501da60556e6a1aedc8f1e38
Preston Phelan <preston.phelan@gmail.com>
2016-12-22 21:10:39 -0800

    Arithmetic buttons, clear button

--- w6d5/skeleton/calculator.jsx
+++ w6d5/skeleton/calculator.jsx
@@ -11,22 +11,58 @@ class Calculator extends React.Component {
 
     this.setNum1 = this.setNum1.bind(this);
     this.setNum2 = this.setNum2.bind(this);
+    this.add = this.add.bind(this);
+    this.subtract = this.subtract.bind(this);
+    this.multiply = this.multiply.bind(this);
+    this.divide = this.divide.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
   setNum1(e) {
-    if (e.length > 0) {
-      this.state.num1 = parseInt(e);
+    let field = e.currentTarget.value;
+    if (field.length > 0) {
+      this.setState({num1: parseInt(field)});
     } else {
-      this.state.num1 = 0;
+      this.setState({num1: 0});
     }
   }
 
   setNum2(e) {
-    if (e.length > 0) {
-      this.state.num2 = parseInt(e);
+    let field = e.currentTarget.value;
+    if (field.length > 0) {
+      this.setState({num2: parseInt(field)});
     } else {
-      this.state.num2 = 0;
+      this.setState({num2: 0});
     }
+  }
+
+  add(e) {
+    e.preventDefault();
+    this.setState({result: this.state.num1 + this.state.num2});
+  }
+
+  subtract(e) {
+    e.preventDefault();
+    this.setState({result: this.state.num1 - this.state.num2});
+  }
+
+  multiply(e) {
+    e.preventDefault();
+    this.setState({result: this.state.num1 * this.state.num2});
+  }
+
+  divide(e) {
+    e.preventDefault();
+    this.setState({result: this.state.num1 / this.state.num2});
+  }
+
+  reset(e) {
+    e.preventDefault();
+    this.setState({
+      result: 0,
+      num1: "",
+      num2: ""
+    });
   }
 
   render() {
@@ -34,8 +70,14 @@ class Calculator extends React.Component {
       <div>
         <h1>{this.state.result}</h1>
         <br />
-        <input onChange={this.setNum1}></input>
-        <input onChange={this.setNum2}></input>
+        <input onChange={this.setNum1} value={this.state.num1}></input>
+        <input onChange={this.setNum2} value={this.state.num2}></input>
+        <button onClick={this.reset}>Clear</button>
+        <br />
+        <button onClick={this.add}>+</button>
+        <button onClick={this.subtract}>-</button>
+        <button onClick={this.multiply}>*</button>
+        <button onClick={this.divide}>/</button>
       </div>
     );
   }
