897973886bf9356570ee81fefb39c14d67f63211
Preston Phelan <preston.phelan@gmail.com>
2017-01-03 20:50:04 -0800

    Flesh out Giphy basics: actions, reducer, rootReducer, and store written

--- w7d3/giphy/giphy_search.jsx
+++ w7d3/giphy/giphy_search.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import configureStore from './store/store';
+import { configureStore } from './store/store';
 import Root from './components/root';
 import { fetchSearchGiphys } from './util/api_util.js';
+import { receiveSearchGiphys } from './actions/giphy_actions';
 
-// window.fetchSearchGiphys = fetchSearchGiphys;
+const store = configureStore();
+window.store = store;
+window.fetchSearchGiphys = fetchSearchGiphys;
+window.receiveSearchGiphys = receiveSearchGiphys;
+
+document.addEventListener('DOMContentLoaded', () => {
+});
